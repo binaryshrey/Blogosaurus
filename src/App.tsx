fe805b762b3649542bdbe88b1ba4dcbf666875ff
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2024-07-30 21:19:12 +0530

    adds login comp

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,28 @@ import Home from './components/home/Home';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PageNotFound from './components/not-found/PageNotFound';
+import { AuthContextProvider } from './hooks/AuthContext';
+
+const Login = React.lazy(() => import('./components/login/Login'));
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
+        <AuthContextProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<PageNotFound />} />
+            <Route
+              path="/login"
+              element={
+                <React.Suspense fallback={<>Login</>}>
+                  <Login />
+                </React.Suspense>
+              }
+            />
+          </Routes>
+        </AuthContextProvider>
       </BrowserRouter>
     </>
   );
---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGESENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
