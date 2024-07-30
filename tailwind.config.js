9381bcbdd3e46b9c3888f9cf9e6c7a46fd4b67d9
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2024-07-30 18:47:54 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+      extend: {
+        colors: {
+          teal: colors.teal,
+          cyan: colors.cyan,
+        },
+        fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        animation: {
+          gradient: "gradient 8s linear infinite",
+        },
+        keyframes: {
+          gradient: {
+            to: {
+              backgroundPosition: "var(--bg-size) 0",
+            },
+          },
+        },
+      },
+      },
+    },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio')
+  ],
+}
