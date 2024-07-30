bad43100cc08caed9af3b4617ecdcd07f5f336ae
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2024-07-30 19:55:22 +0530

    add CTA

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroSection from './HeroSection';
 import Features from './Features';
+import CTA from './CTA';
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <Features />
+      <CTA />
     </>
   );
 };
